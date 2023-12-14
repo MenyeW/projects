68b39b77283f5555665a857587e5c40f79142a12
Rahul Bhatnagar <ralbhat@gmail.com>
2023-12-14 21:26:04 +0100

    Added some components and routes

--- apps/resume/src/app/app.routes.ts
+++ apps/resume/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
+import { OverviewComponent } from './components/overview/overview.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', component: OverviewComponent },
+  { path: 'skills', component: SkillsComponent },
+];
