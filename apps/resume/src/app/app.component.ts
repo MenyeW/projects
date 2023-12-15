d5605e843ccc0434a01a933485e4724b85ae0b78
Rahul Bhatnagar <ralbhat@gmail.com>
2023-12-15 13:42:47 +0100

    Sidebar is better than header

--- apps/resume/src/app/app.component.ts
+++ apps/resume/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent, RouterModule],
+  imports: [SidebarComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
