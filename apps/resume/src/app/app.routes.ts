e8258919cf429881814e3c4154fc8c734d3aa923
Pixop <mepixop@gmail.com>
2023-12-16 01:49:40 +0100

    Completed component layouts, overview content sans translations

--- apps/resume/src/app/app.routes.ts
+++ apps/resume/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Route } from '@angular/router';
 import { OverviewComponent } from './components/overview/overview.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { HomeComponent } from './components/home/home.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 export const appRoutes: Route[] = [
-  { path: '', component: OverviewComponent },
-  { path: 'skills', component: SkillsComponent },
+  {
+    path: '', component: HomeComponent, children: [
+      { path: '', component: OverviewComponent },
+      { path: 'skills', component: SkillsComponent },
+      { path: 'experience', component: ExperienceComponent }
+    ]
+  },
 ];
