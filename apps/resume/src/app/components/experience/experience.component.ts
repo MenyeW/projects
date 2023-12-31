fb8349a3f9866a7a7ad34d42224a7a78270f9749
Rahul Bhatnagar <ralbhat@gmail.com>
2023-12-31 12:45:09 +0100

    Added translations for experience page

--- apps/resume/src/app/components/experience/experience.component.ts
+++ apps/resume/src/app/components/experience/experience.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { TranslatorService } from '../../lib/services/translator.service';
 
 @Component({
   selector: 'app-experience',
@@ -8,4 +10,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css',
 })
-export class ExperienceComponent {}
+export class ExperienceComponent implements OnDestroy {
+  locale: Record<string, string>;
+  localeSubscription: Subscription;
+
+  constructor(private translatorService: TranslatorService) {
+    this.locale = translatorService.getLocaleStrings('experience');
+    this.localeSubscription = translatorService.updateLocale.subscribe(
+      (strings) => {
+        this.locale = strings['experience'];
+        console.log(this.locale);
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.localeSubscription.unsubscribe();
+  }
+}
